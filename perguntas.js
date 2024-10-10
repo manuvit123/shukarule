9c6f12a872c429dfcf8c93946069189af6ccde5f
manuvit123 <146022177+manuvit123@users.noreply.github.com>
2024-10-10 12:10:41 -0300

    c

---
+++ perguntas.js
@@ -0,0 +1,99 @@
+criaCartao(
+    'Geografia',
+    'Qual o maior oceano do mundo?',
+    'Resposta: Oceano Pacífico')
+    
+    criaCartao(
+    'Geografia',
+    'Qual pais tem a maior populacao?',
+    'Resposta: China')
+    
+    criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'Resposta: Paris')
+    
+    criaCartao(
+    'Geografia',
+    'Onde está localizada a Grande Barreira de Coral?',
+    'Resposta: Austrália')
+    
+    criaCartao(
+    'Geografia',
+    'Qual o rio mais longo do mundo?',
+    'Resposta: Rio Nilo')
+    
+    criaCartao(
+    'Geografia',
+    'Qual país é conhecido como a Terra do Sol Nascente?',
+    'Resposta: Japão')
+    
+    criaCartao(
+    'Geografia',
+    'Qual a maior cadeia de montanhas do mundo?',
+    'Resposta: Himalaias')
+    
+    criaCartao(
+    'Geografia',
+    'Qual deserto é o maior do mundo?',
+    'Resposta: Deserto da Antártica')
+    
+    criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'Resposta: Brasília')
+    
+    criaCartao(
+    'Geografia',
+    'Onde fica o Canal do Panamá?',
+    'Resposta: Entre o Oceano Atlântico e o Pacífico')
+    
+    criaCartao(
+    'Geografia',
+    'Qual país tem o maior território?',
+    'Resposta: Rússia')
+    
+    criaCartao(
+    'Geografia',
+    'Qual a capital da Índia?',
+    'Resposta: Nova Délhi')
+    
+    criaCartao(
+    'Geografia',
+    'Qual país possui a maior área de floresta tropical?',
+    'Resposta: Brasil')
+    
+    criaCartao(
+    'Geografia',
+    'Qual o nome do ponto mais alto da Terra?',
+    'Resposta: Monte Everest')
+    
+    criaCartao(
+    'Geografia',
+    'Qual o país mais pequeno do mundo?',
+    'Resposta: Vaticano')
+    
+    criaCartao(
+    'Geografia',
+    'Qual o maior lago do mundo por volume de água?',
+    'Resposta: Lago Baikal')
+    
+    criaCartao(
+    'Geografia',
+    'Qual o continente mais seco?',
+    'Resposta: Antártida')
+    
+    criaCartao(
+    'Geografia',
+    'Qual a capital da Argentina?',
+    'Resposta: Buenos Aires')
+    
+    criaCartao(
+    'Geografia',
+    'Qual o principal rio da América do Sul?',
+    'Resposta: Rio Amazonas')
+    
+    criaCartao(
+    'Geografia',
+    'Qual o maior arquipélago do mundo?',
+    'Resposta: Indonésia')
