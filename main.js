9db0b959635d075204352c3dede814ab554979c5
manuvit123 <146022177+manuvit123@users.noreply.github.com>
2024-10-30 07:46:18 -0300

    c

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ let respostaEstaVisivel = false;
 
 function viraCartao(){
     respostaEstaVisivel = !respostaEstaVisivel;
-    cartao.classList.toggle('acticve', respostaEstaVisivel;)
+    cartao.classList.toggle('active', respostaEstaVisivel);
 }
 
 cartao.addEventListener('click', viraCartao);
