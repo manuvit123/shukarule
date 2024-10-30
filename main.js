f14a9201ffceda3eae28d9bd7adafa4d24688f1e
manuvit123 <146022177+manuvit123@users.noreply.github.com>
2024-10-30 07:41:30 -0300

    c

--- main.js
+++ main.js
@@ -14,10 +14,13 @@ function criaCartao(categoria, pergunta, resposta) {
 </div>
     `
 
-let respostaEstaVisível = false;
+let respostaEstaVisivel = false;
 function viraCartao(){
-    
+    respostaEstaVisivel = !respostaEstaVisivel;
+    cartao.classList.toggle('active', respostaEstaVisivel;)
 }
+
+cartao.addEventListener('click', viraCartao);
 
     container.appendChild(cartao);
 }
