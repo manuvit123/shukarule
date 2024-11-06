dbdb747e0e6c237082e5ff3a4e3c908d6c3674be
manuvit123 <146022177+manuvit123@users.noreply.github.com>
2024-11-06 07:55:15 -0300

    m

--- pagina2.js
+++ pagina2.js
@@ -18,11 +18,11 @@ function criaCartao(categoria, pergunta, resposta) {
 }
 
 function mostraResposta (card){
-    const resposta=card.querySelector('resposta-cartao');
+    const resposta=card.querySelector('.resposta-cartao');
     if(resposta.style.display==='none'){
         resposta.style.display='block';
     }
     else{
-        resposta.style.diplay='none'
+        resposta.style.display='none';
     }
 }
